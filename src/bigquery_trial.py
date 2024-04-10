f4edfbcb40b1aca61b2527f5c779554afdd52466
lsy8117 <liusy5534@gmail.com>
2024-04-10 17:04:09 +0800

    Update revenue prediction functions
    New prediction model created, with around 0.7 R-square value. Pending to be linked with dashboard.

--- src/bigquery_trial.py
+++ src/bigquery_trial.py
@@ -3,6 +3,7 @@ import os
 import pandas as df
 
 
+
 # for access to bigquery data base, need to generate a service account
 # download the service account credentials in json format
 # this would be used to access bigquery
@@ -47,9 +48,36 @@ def create_model():
         from
         IS3107.movie_details
         """)
+    
+    # alternative create model query to be used, R-squared is much higher
+    """"
+      CREATE MODEL `firm-catalyst-417613.IS3107.test_model_4`
+      OPTIONS(model_type = 'RANDOM_FOREST_REGRESSOR')
+      AS 
+      SELECT
+      revenue as label,
+      budget,
+      release_date,
+      runtime,
+      cast1_popularity,
+      cast2_popularity,
+      director_popularity,
+      producer_popularity,
+      tmdb_popularity,
+      tmdb_vote_average,
+      tmdb_vote_count,
+      -- published_at,
+      view_count,
+      like_count,
+      comment_count,
+      avg_popularity_before_2020
+      from
+      IS3107.training_v1
+    """
     query_job = client.query(QUERY)
     output = query_job.result()
     print(output)
+
 
 # try creating a model directly using movie_details data
 create_model()
@@ -76,4 +104,28 @@ SELECT
   cast(producer_id as string) as producer_id
 from
   IS3107.movie_details
+"""
+
+###### Bigquery Code for Prediction ######
+## Query to predict revenue use an already trained model, can manually subs in the data input for prediction
+"""
+SELECT
+  predicted_label as predicted_revenue
+FROM
+ML.PREDICT(MODEL `IS3107.test_model_4`, (
+  SELECT
+    50000 AS budget,
+    cast(null as date) AS release_date,
+    124 AS runtime,
+    10 AS cast1_popularity,
+    null AS cast2_popularity,
+    6 AS director_popularity,
+    1.2 AS producer_popularity,
+    25 AS tmdb_popularity,
+    7 AS tmdb_vote_average,
+    1789 AS tmdb_vote_count,
+    2209901 AS view_count,
+    17584 AS like_count,
+    1019 AS comment_count,
+    null AS avg_popularity_before_2020))
 """
