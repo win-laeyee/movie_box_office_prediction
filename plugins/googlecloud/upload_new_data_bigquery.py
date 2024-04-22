ee89880170e52274c4df8d584a982266a760dd4f
faithtanlh <faithtanlihui@gmail.com>
2024-04-23 03:22:47 +0800

    Added creation of staging table & upsert function via BQ merge

--- plugins/googlecloud/upload_new_data_bigquery.py
+++ plugins/googlecloud/upload_new_data_bigquery.py
@@ -1,6 +1,8 @@
 import os
 from google.cloud import bigquery
-from datetime import datetime
+from create_table_bigquery import create_table_if_not_exists
+from datetime import datetime, timedelta
+
 
 def upload_df_to_temp_table(project_id, dataset_id, table_id, schema, df, mode):
     """
@@ -19,16 +21,14 @@ def upload_df_to_temp_table(project_id, dataset_id, table_id, schema, df, mode):
     script_dir = os.path.dirname(os.path.realpath(__file__))
     json_path = os.path.join(script_dir, "is3107-418809-62c002a9f1f7.json")
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
-
     client = bigquery.Client(project=project_id)
     
-    table_id = f"{project_id}.{dataset_id}.{table_id}"
-    table = bigquery.Table(table_id, schema=schema)
-    # Set the table expiration time
-    expiration_time = datetime.utcnow() + timedelta(days=1)  
+    # Construct temporary table based on schema
+    table_ref = f"{project_id}.{dataset_id}.{table_id}"
+    table = bigquery.Table(table_ref, schema=schema)
+    expiration_time = datetime.now() + timedelta(days=1) # Set the table expiration time
     table.expires = expiration_time
-    table = client.create_table(table, timeout=30)
-    table_ref = client.dataset(dataset_id).table(table_id)
+    create_table_if_not_exists(project_id, dataset_id, table_id, schema)
     
     # Define job configuration
     job_config = bigquery.LoadJobConfig()
@@ -40,22 +40,20 @@ def upload_df_to_temp_table(project_id, dataset_id, table_id, schema, df, mode):
         job_config.write_disposition = bigquery.WriteDisposition.WRITE_EMPTY  # Write only when tables empty - ensure no any overwrite
 
     df['insertion_datetime'] = datetime.now() # create new column if not exist and save to same file
-    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
     job.result() # Wait for the job to complete
+    print(f"Dataframe uploaded to temporary table {table_id} in dataset {dataset_id} successfully.")
 
-    print(f"Dataframe uploaded to table {table_id} in dataset {dataset_id} successfully.")
-
-    
-def upsert_row_to_table(project_id, dataset_id, table_id, primary_key_columns, new_row_values):
+def upsert_df_to_table(project_id, dataset_id, table_id, primary_key_columns, df):
     """
-    Upsert a row in the specified BigQuery table.
+    Upsert a pandas Dataframe to the specified BigQuery table.
 
     Args:
         project_id (str): The ID of the Google Cloud project.
         dataset_id (str): The ID of the BigQuery dataset.
         table_id (str): ID of the BigQuery table.
         primary_key_columns (list of str): List of column names that form the primary key.
-        new_row_values (dict): Dictionary containing column names and their corresponding values for the new row.
+        df (pandas.DataFrame): Dictionary containing column names and their corresponding values for the new row.
 
     Returns:
         None
@@ -67,35 +65,34 @@ def upsert_row_to_table(project_id, dataset_id, table_id, primary_key_columns, n
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
     client = bigquery.Client(project=project_id)
     table_dest = f"{project_id}.{dataset_id}.{table_id}"
-
-    # Construct query to check if row already exists
-    where_conditions = ' AND '.join(f"{col} = @{col}" for col in primary_key_columns)
-    select_query = f"SELECT * FROM `{table_dest}` WHERE {where_conditions}"
-
-    # Set query parameters
-    query_params = [bigquery.ScalarQueryParameter(name, "STRING", value) for name, value in new_row_values.items()]
-    
-    # Run the select query
-    query_job = client.query(select_query, job_config=bigquery.QueryJobConfig(query_parameters=query_params))
-    existing_row = query_job.result()
+    table_src = f"{project_id}.staging_dataset.{table_id}"
 
     # Add insertion datetime column to row
-    new_row_values['insertion_datetime'] = datetime.now()
+    df["insertion_datetime"] = datetime.now()
 
-    if existing_row:
-        # If the row exists, update it
-        update_values = ', '.join(f"{col} = @{col}" for col in new_row_values.keys())
-        update_query = f"UPDATE `{table_dest}` SET {update_values} WHERE {where_conditions}"
+    # Construct SQL merge statement
+    source_columns = df.columns.tolist()
+    target_columns = list(filter(lambda i: i not in primary_key_columns, source_columns))
+    merge_sql = f"""
+        MERGE `{table_dest}` AS target
+        USING `{table_src}` AS source
+        ON {', '.join([f'target.{col} = source.{col}' for col in primary_key_columns])}        
+        WHEN MATCHED THEN
+            UPDATE SET {', '.join([f'target.{col} = source.{col}' for col in target_columns])}
+        WHEN NOT MATCHED THEN
+            INSERT ({', '.join(source_columns)})
+            VALUES ({', '.join(source_columns)})
+    """
+    
+    # Get original table schema
+    original_table_ref = client.dataset(dataset_id).table(table_id)
+    original_table = client.get_table(original_table_ref)
+    original_table_schema = original_table.schema
 
-        # Run the update query
-        query_job = client.query(update_query, job_config=bigquery.QueryJobConfig(query_parameters=query_params))
-        print("Row updated successfully.")
-    else:
-        # If the row doesn't exist, insert it
-        columns = ', '.join(new_row_values.keys())
-        values = ', '.join(f"@{col}" for col in new_row_values.keys())
-        insert_query = f"INSERT INTO `{table_dest}` ({columns}) VALUES ({values})"
+    # Populate temp table
+    upload_df_to_temp_table(project_id, "staging_dataset", table_id, original_table_schema, df, mode="truncate")
 
-        # Run the insert query
-        query_job = client.query(insert_query, job_config=bigquery.QueryJobConfig(query_parameters=query_params))
-        print("Row inserted successfully.")
+    # Execute SQL merge statement
+    query_job = client.query(merge_sql)
+    query_job.result()
+    print("Upsert operation completed successfully.")
